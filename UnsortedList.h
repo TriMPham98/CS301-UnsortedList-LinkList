9c10992494305f20cada282a6fd13eecebf4ad14
Tri <trimpham98@gmail.com>
2023-02-01 13:04:26 -0800

    Add struct declaration for Node

--- UnsortedList.h
+++ UnsortedList.h
@@ -55,7 +55,14 @@ public:
   //       item is a copy of element at current position.
 
 private:
+  struct Node {
+      T value;
+      Node * next;
+  };
+
   int length;
+  Node *head;
+  Node *currPos;
 
   // Need to create the head... and a node definition
   // Also need something to track the iterator position
